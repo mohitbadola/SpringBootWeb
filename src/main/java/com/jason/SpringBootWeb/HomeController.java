a782cbfd197c5fd55be5a5ba74e6c31d26c3fa82
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2024-08-30 21:32:52 +0530

    Create HomeController and map it

---
+++ src/main/java/com/jason/SpringBootWeb/HomeController.java
@@ -0,0 +1,15 @@
+package com.jason.SpringBootWeb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        System.out.println("home method called");
+        return "index.jsp";
+    }
+
+}
