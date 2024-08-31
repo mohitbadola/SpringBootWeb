b33c4db01a13e13636c75a7d8ac01f66fe43e14f
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2024-09-01 01:26:17 +0530

    Update HomeController

--- src/main/java/com/jason/SpringBootWeb/HomeController.java
+++ src/main/java/com/jason/SpringBootWeb/HomeController.java
@@ -15,9 +15,7 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public String add(HttpServletRequest req, HttpSession session){
-        int num1 = Integer.parseInt(req.getParameter("num1"));
-        int num2 = Integer.parseInt(req.getParameter("num2"));
+    public String add(int num1, int num2, HttpSession session){
         int result = num1 + num2;
         session.setAttribute("result", result);
         return "result.jsp";
