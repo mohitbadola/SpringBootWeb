74ea20a2ab5caf772416ed98feb4ce649ceba7f4
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2024-09-01 16:11:35 +0530

    Use ModelAttribute

--- src/main/java/com/jason/SpringBootWeb/HomeController.java
+++ src/main/java/com/jason/SpringBootWeb/HomeController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -27,14 +28,8 @@ public class HomeController {
     }
 
     @RequestMapping("addUser")
-    public ModelAndView add(@RequestParam("uid") int uid, @RequestParam("uname") String uname, ModelAndView mv){
-
-        User user = new User();
-        user.setUid(uid);
-        user.setUname(uname);
-        mv.addObject("user", user);
-        mv.setViewName("result");
-        return mv;
+    public String add(@ModelAttribute User user){
+        return "result";
     }
 
 }
